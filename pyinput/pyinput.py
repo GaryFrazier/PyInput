4cacd8e0297eb0753dbad1f3203688e4228bf180
frazier_g <garyfrazier95@yahoo.com>
2022-03-26 09:37:05 -0400

    add doocumentation

--- pyinput/pyinput.py
+++ pyinput/pyinput.py
@@ -1,15 +1,6 @@
-import sched
 from time import sleep, time
+import pyinputkeycodes
 import win32gui, win32ui, win32con, win32api
-
-# http://www.kbdedit.com/manual/low_level_vk_list.html
-VK_KEY_W = 0x57
-VK_KEY_A = 0x41
-VK_KEY_S = 0x53
-VK_KEY_D = 0x44
-VK_KEY_P = 0x50
-VK_SHIFT = 0xA0
-VK_RETURN = 0x0D
 
 def list_windows():
     def winEnumHandler(hwnd, ctx):
@@ -31,27 +22,11 @@ def list_inner_windows(whndl):
 def get_handle(windowName):
     return win32gui.FindWindow(None, windowName)
 
-def press_key(hwnd, key, hold_sec):
+def press_key(hwnd, key, hold_sec = 0.1, force_foreground = True):
     #Todo: threading
-    win32gui.SetForegroundWindow(hwnd)
+    if (force_foreground):
+        win32gui.SetForegroundWindow(hwnd)
+    
     win32api.SendMessage(hwnd, win32con.WM_KEYDOWN, key, 0)
     sleep(hold_sec);
     win32api.SendMessage(hwnd, win32con.WM_KEYUP, key, 0)
-            
-
-#Todo delete
-def main():
-    
-    if 0:
-        #list_windows()
-        list_inner_windows(1967572)
-    else:
-        # init window handle
-        window_name = "Binding of Isaac: Repentance"
-        hwnd = get_handle(window_name)
-        
-        press_key(hwnd, VK_RETURN, 0.1)
-        press_key(hwnd, VK_KEY_W, 2)
-
-    return
-main()
